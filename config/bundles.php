05fb7f7b65f79f15727d2d628bd303e8a2226261
Víctor.L <vlopez@quadram.mobi>
2022-09-04 12:16:11 +0200

    Set up beHat, move feature test to Application and Context to Infrastructure

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
---
+++ tests/app/features/Brewers/Beers/beer_search.feature
@@ -0,0 +1,8 @@
+#Feature:
+#  In order
+#  As a user
+#  I expect to get a list of beers
+#  Scenario: A list of beers
+#    Given I send a GET request to "/api/v1/beers"
+#    Then the response status code should be 200
+#    And the response should be an array
